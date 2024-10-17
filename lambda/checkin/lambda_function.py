e70be1408d41a0cd2b2f55b289111501e55f70eb
El Cello <marcel.neumann@draeger.com>
2024-10-17 12:05:28 +0200

    return given query param

--- lambda/checkin/lambda_function.py
+++ lambda/checkin/lambda_function.py
@@ -1,8 +1,18 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+
+    query_params = event.get('queryStringParameters', {})
+    userId = query_params.get('guid', None)
+
+    if userId:
+        statusCode = 200
+        responseMessage = "Received userId " + str(userId}.
+    else:
+        statusCode = 400
+        responseMessage = 'No userId was provided.'
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hi there from Lambda!')
+        "statusCode": statusCode,
+        "body": json.dumps(responseMessage)
     }
