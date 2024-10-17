ceb358ef19617c984dc8bcc9a6799a2a97175b29
El Cello <marcel.neumann@draeger.com>
2024-10-17 12:56:34 +0200

    put to db

--- lambda/checkin/lambda_function.py
+++ lambda/checkin/lambda_function.py
@@ -1,18 +1,36 @@
 import json
+import boto3
+from datetime import datetime
 
 def lambda_handler(event, context):
 
-    query_params = event.get('queryStringParameters', {})
-    userId = query_params.get('guid', None)
+    query_params = event.get("queryStringParameters", {})
+    id = query_params.get("id", None)
 
-    if userId:
-        statusCode = 200
-        responseMessage = "Received userId " + str(userId) + "."
+    if id:
+        
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table("members")
+        
+        try:
+            table.put_item(
+                Item={
+                    "id": str(id),
+                    "last-checkin": datetime.utcnow().isoformat()
+            }
+        )
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps(f"Error persisting check-in!" + str(e))
+        }
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Received id " + str(id) + ".")
+        }
     else:
-        statusCode = 400
-        responseMessage = 'No userId was provided.'
-
-    return {
-        "statusCode": statusCode,
-        "body": json.dumps(responseMessage)
+        return {
+            "statusCode": 400,
+            "body": json.dumps('No id was provided.')
     }
