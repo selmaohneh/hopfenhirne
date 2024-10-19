8a404208e335a52af3d8521331fbc92f37ab73d9
kali <marcel@neumann.sh>
2024-10-19 20:55:00 +0200

    add lambda for adding new user

---
+++ lambda/add-user/add-user.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import uuid
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+    query_params = event.get("queryStringParameters", {})
+    name = query_params.get("name", None)
+    id = uuid.uuid4()
+
+    if id:
+        
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table("members")
+        
+        try:
+            table.put_item(
+                Item={
+                    "id": str(id)
+            }
+        )
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps("Error adding new user " + str(name))
+        }
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Added new user " + str(id))
+        }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps('No name was provided')
+    }
