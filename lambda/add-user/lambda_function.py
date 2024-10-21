7eee13c77b06c9b95c41c3ed5e5285843cf544ca
kali <marcel@neumann.sh>
2024-10-21 20:37:04 +0200

    add number

--- lambda/add-user/lambda_function.py
+++ lambda/add-user/lambda_function.py
@@ -1,39 +1,49 @@
 import json
 import boto3
 import uuid
-from datetime import datetime
 
 def lambda_handler(event, context):
-
     query_params = event.get("queryStringParameters", {})
     name = query_params.get("name", None)
     id = uuid.uuid4()
 
-    if id:
-        
+    if name:
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("members")
         
         try:
+            scan = table.scan()
+            members = scan.get('Items', [])
+            nextNumber = -1 
+
+            for member in members:
+                number = member.get("number", -1)
+                if number > nextNumber:
+                    nextNumber = number
+            
+            
+            nextNumber += 1
+
             table.put_item(
                 Item={
                     "id": str(id),
                     "name": str(name),
-                    "participations": 0
-            }
-        )
+                    "participations": 0,
+                    "number": nextNumber
+                }
+            )
         except Exception as e:
             return {
                 'statusCode': 500,
-                'body': json.dumps("Error adding new user " + str(name))
-        }
+                'body': json.dumps("Error adding new user " + str(name) + ": " + str(e))
+            }
         
         return {
             "statusCode": 200,
-            "body": json.dumps("Added new user " + str(id))
+            "body": json.dumps("Added new user " + str(id) + " with number " + str(nextNumber))
         }
     else:
         return {
-            "statusCode": 400,
-            "body": json.dumps('No name was provided')
-    }
+            'statusCode': 400,
+            'body': json.dumps('No name was provided')
+        }
