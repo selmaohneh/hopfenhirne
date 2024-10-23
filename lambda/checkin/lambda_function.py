263655e44a33841c9953892f47132ccad771a0c4
kali <marcel@neumann.sh>
2024-10-23 20:48:13 +0200

    check id, refactor logic

--- lambda/checkin/lambda_function.py
+++ lambda/checkin/lambda_function.py
@@ -8,36 +8,37 @@ def lambda_handler(event, context):
     query_params = event.get("queryStringParameters", {})
     id = query_params.get("id", None)
 
-    if id:
-        
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table("members")
-        
-        timezone = pytz.timezone('Europe/Berlin')
-        lastCheckin = datetime.now(timezone).date().isoformat()
-        
-        try:
-            table.update_item(
-                Key={
-                    "id": str(id)
-                },
-                UpdateExpression="SET lastCheckin = :lastCheckin",
-                ExpressionAttributeValues={
-                    ":lastCheckin": lastCheckin
-                }
-            )
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps(f"Error persisting check-in!")
-        }
-        
-        return {
-            "statusCode": 200,
-            "body": json.dumps("Checked-in member with id " + str(id))
-        }
-    else:
+    if not id:
         return {
             "statusCode": 400,
             "body": json.dumps('No id was provided.')
+        }
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("members")
+        
+    timezone = pytz.timezone('Europe/Berlin')
+    lastCheckin = datetime.now(timezone).date().isoformat()
+        
+    getResponse = table.get_item(Key={"id": str(id)})
+
+    if 'Item' not in getResponse:
+        return {
+            "statusCode": 404,
+            "body": json.dumps(f"Member with id {id} does not exist.")
+        }
+
+    table.update_item(
+        Key={
+            "id": str(id)
+        },
+        UpdateExpression="SET lastCheckin = :lastCheckin",
+        ExpressionAttributeValues={
+            ":lastCheckin": lastCheckin
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Checked-in member with id " + str(id))
     }
