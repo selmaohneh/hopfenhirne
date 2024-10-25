749539abf78a9e7b3c3775aeb4a7f6dd21433a72
kali <marcel@neumann.sh>
2024-10-25 18:08:11 +0200

    add daily lambda

---
+++ lambda/daily/lambda_function.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+from datetime import datetime
+import pytz
+
+def lambda_handler(event, context):
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("members")
+        
+    timezone = pytz.timezone('Europe/Berlin')
+    today = datetime.now(timezone).date().isoformat()
+        
+    response = table.scan(
+        FilterExpression="checkin = :today",
+        ExpressionAttributeValues={
+            ":today": today
+        }
+    )
+    
+    members = response.get('Items', [])
+    
+   for member in members:
+        id = member['id']
+        participations = member.get('participations')
+        
+        table.update_item(
+            Key={
+                "id": id
+            },
+            UpdateExpression="SET participations = :newParticipations",
+            ExpressionAttributeValues={
+                ":newParticipations": participations + 1
+            }
+        )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Handled daily check for event")
+    }
